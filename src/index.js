5b59341ecc107ebe0f71f79131933216df5c6fa8
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-22 01:16:36 +0300

    swtich,ing tab module logic v1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+// import { first } from "./home";
 import style from "./style.css";
 // import image from "./tpizza-1.jpg";
-import name from "./home.js";
 
-name();
+import home from "./home";
+import menu from "./menu";
+
+home();
+
+document.body.append(home());
+
+const homeTab = document.querySelector(".home");
+const menuTab = document.querySelector(".menu");
+const contactTab = document.querySelector(".contact");
+const textDiv = document.querySelector(".text-container");
+
+menuTab.addEventListener("click", function () {
+  textDiv.remove();
+  if (!denden.textContent === "zaaa") document.body.append(menu());
+});
